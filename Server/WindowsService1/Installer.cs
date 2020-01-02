5869a790a7cbfb7c1ece86b8ac1468ffc3c651b4
Marcin Cyc <marcinbedcyc@mat.umk.pl>
2020-01-02 21:18:59 +0100

    Add initial server

---
+++ Server/WindowsService1/Installer.cs
@@ -0,0 +1,35 @@
+﻿namespace RestWindowsService
+{
+    #region Namespaces
+    using System.ComponentModel;
+    using System.ServiceProcess;
+    #endregion
+
+    [RunInstaller(true)]
+    public partial class Installer : System.Configuration.Install.Installer
+    {
+        private ServiceProcessInstaller process;
+        private ServiceInstaller service;
+
+        public Installer()
+        {
+            process = new ServiceProcessInstaller();
+            process.Account = ServiceAccount.LocalSystem;
+            service = new ServiceInstaller();
+            service.ServiceName = "JokesWorldServer";
+            service.Description = "WCF REST API Hosting Jokes on Windows Service";
+            service.DelayedAutoStart = true;
+            Installers.Add(process);
+            Installers.Add(service);
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
