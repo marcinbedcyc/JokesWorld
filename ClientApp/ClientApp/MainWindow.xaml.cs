e60b3a4513fc6ba1bf2e137f1da26422b05f9ea7
Marcin Cyc <marcinbedcyc@mat.umk.pl>
2020-01-15 19:45:32 +0100

    Create Login window

--- ClientApp/ClientApp/MainWindow.xaml.cs
+++ ClientApp/ClientApp/MainWindow.xaml.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Net.Http;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Newtonsoft.Json;
 
 namespace ClientApp
 {
@@ -23,6 +24,43 @@ namespace ClientApp
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                HttpResponseMessage response = await client.GetAsync("https://localhost:44377/users/nickname/" + nicknameField.Text);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                User account = JsonConvert.DeserializeObject<User>(responseBody);
+
+                Console.WriteLine(account.Password);
+                // james@example.com
+                if(BCrypt.Net.BCrypt.Verify(passwordField.Password, account.Password))
+                    status.Content = "Correct";
+                else
+                    status.Content = "Wrong";
+
+
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", ex.Message);
+            }
+        }
+
+        public class User
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Surname { get; set; }
+            public string Email { get; set; }
+            public string Nickname { get; set; }
+            public string Password { get; set; }
         }
     }
 }
