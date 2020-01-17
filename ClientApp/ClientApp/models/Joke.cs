aefcd210932d65e186a16c8704017a55091e7d20
Marcin Cyc <marcinbedcyc@mat.umk.pl>
2020-01-17 23:31:59 +0100

    Add user, joke model

---
+++ ClientApp/ClientApp/models/Joke.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientApp.models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string Content { get; set; }
+
+        public int AuthorFK { get; set; }
+
+        public override string ToString()
+        {
+            return this.Id + " " + this.Title + " " + this.Content + " " +  this.CreatedDate.ToString();
+        }
+    }
+
+}
