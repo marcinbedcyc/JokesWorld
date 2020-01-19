bcd02239e035c2cde6fb16c1d6f4efdb4c63050e
Marcin Cyc <marcinbedcyc@mat.umk.pl>
2020-01-19 16:28:41 +0100

    Add GUI for settings

--- ClientApp/ClientApp/pages/SettingsPage.xaml.cs
+++ ClientApp/ClientApp/pages/SettingsPage.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using ClientApp.models;
 using ClientApp.pages;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,9 +25,88 @@ namespace ClientApp
     public partial class SettingsPage : Page
     {
         public User CurrentLoggedInUser { get; set; }
-        public SettingsPage()
+        public SettingsPage(User user)
         {
+            this.CurrentLoggedInUser = user;
             InitializeComponent();
+            NameTextBox.Text = this.CurrentLoggedInUser.Name;
+            SurnameTextBox.Text = this.CurrentLoggedInUser.Surname;
+            EMailTextBox.Text = this.CurrentLoggedInUser.Email;
+            NicknameTextBox.Text = this.CurrentLoggedInUser.Nickname;
+            OldPasswordBox.Password = "";
+            NewPasswordBox.Password = "";
+            ReapeatPasswordBox.Password = "";
+            GetComments();
+            GetJokes();
         }
+
+        private void SaveChangeButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        async private void GetComments()
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                HttpResponseMessage response = await client.GetAsync("https://localhost:44377/api/users/" + CurrentLoggedInUser.Id + "/comments");
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                int count = 0;
+                var comments = JsonConvert.DeserializeObject<List<Comment>>(responseBody);
+                foreach (Comment c in comments)
+                {
+                    if (count % 2 == 0)
+                        CommentsStackPanel.Children.Add(Utils.CreateCommentContentGrid(c, true, new RoutedEventHandler((s, e) => CommentButton_Click(s, e, c))));
+                    else
+                        CommentsStackPanel.Children.Add(Utils.CreateCommentContentGrid(c, false, new RoutedEventHandler((s, e) => CommentButton_Click(s, e, c))));
+                    count++;
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        async private void GetJokes()
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                HttpResponseMessage response = await client.GetAsync("https://localhost:44377/api/users/" + CurrentLoggedInUser.Id + "/jokes");
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                int count = 0;
+                var jokes = JsonConvert.DeserializeObject<List<Joke>>(responseBody);
+                foreach (Joke j in jokes)
+                {
+                    if (count % 2 == 0)
+                        JokesStackPanel.Children.Add(Utils.CreateJokeContentGrid(j, true, new RoutedEventHandler((s, e) => JokeButton_Click(s, e, j))));
+                    else
+                        JokesStackPanel.Children.Add(Utils.CreateJokeContentGrid(j, false, new RoutedEventHandler((s, e) => JokeButton_Click(s, e, j))));
+                    count++;
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void JokeButton_Click(object sender, RoutedEventArgs e, Joke j)
+        {
+            MessageBox.Show("Not implemented");
+        }
+
+        private void CommentButton_Click(object sender, RoutedEventArgs e, Comment c)
+        {
+            MessageBox.Show("Not implemented");
+        }
+
+
     }
 }
