fc142702413266fab76e234330bdc39a7cc289b2
Marcin Cyc <marcinbedcyc@mat.umk.pl>
2020-01-20 11:21:39 +0100

    Init single user page

---
+++ ClientApp/ClientApp/pages/UserPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using ClientApp.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClientApp.pages
+{
+    /// <summary>
+    /// Interaction logic for UserPage.xaml
+    /// </summary>
+    ///
+    
+    public partial class UserPage : Page
+    {
+        readonly Page previousPage;
+        public User User { get; set; }
+        public UserPage(User user, Page previousPage)
+        {
+            this.User = user;
+            this.previousPage = previousPage;
+            InitializeComponent();
+        }
+    }
+}
