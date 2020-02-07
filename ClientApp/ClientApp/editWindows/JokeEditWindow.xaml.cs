58f032bea57598cec3257a9ce7ab81e19ca6b6aa
Marcin Cyc <marcinbedcyc@mat.umk.pl>
2020-02-07 20:50:10 +0100

    Add docuemntation for JokeEditWindow

--- ClientApp/ClientApp/editWindows/JokeEditWindow.xaml.cs
+++ ClientApp/ClientApp/editWindows/JokeEditWindow.xaml.cs
@@ -23,7 +23,14 @@ namespace ClientApp.editWindows
     /// </summary>
     public partial class JokeEditWindow : Window
     {
+        /// <summary>
+        /// Current logged user in application.
+        /// </summary>
         public User CurrentLoggedInUser { get; set; }
+
+        /// <summary>
+        /// Current joke.
+        /// </summary>
         public Joke CurrentJoke { get; set; }
         public JokeEditWindow(User user, Joke joke)
         {
@@ -35,8 +42,14 @@ namespace ClientApp.editWindows
             ContentTextBox.Text = CurrentJoke.Content;
             CreatedDatePicker.SelectedDate = CurrentJoke.CreatedDate;
         }
-        
 
+        public JokeEditWindow() { }
+
+        /// <summary>
+        /// Save changes in joke when everythins is correct typed in.
+        /// </summary>
+        /// <param name="sender">The control/object that raised the event.</param>
+        /// <param name="e">Event Data.</param>
         private async void AddButton_Click(object sender, RoutedEventArgs e)
         {
 
@@ -44,7 +57,7 @@ namespace ClientApp.editWindows
             List<Joke> jokes = new List<Joke>();
             try
             {
-                HttpResponseMessage response = await client.GetAsync("https://localhost:44377/api/jokes/");
+                HttpResponseMessage response = await client.GetAsync(ConfigurationManager.AppSettings["ServerURL"] + "jokes/");
                 response.EnsureSuccessStatusCode();
                 string responseBody = await response.Content.ReadAsStringAsync();
                 jokes = JsonConvert.DeserializeObject<List<Joke>>(responseBody);
@@ -55,13 +68,13 @@ namespace ClientApp.editWindows
             }
 
             if (jokes.Where(u => u.Content.Equals(ContentTextBox.Text)).Any() && !ContentTextBox.Text.Equals(CurrentJoke.Content)) { MessageBox.Show("Dowcip o podanej treści już istnieje!"); return; }
-            //if (CreatedDatePicker.SelectedDate > DateTime.Now){ MessageBox.Show("Wybierz wcześniejszą datę!"); return; }
+            if (CreatedDatePicker.SelectedDate > DateTime.Now){ MessageBox.Show("Wybierz wcześniejszą datę!"); return; }
             if(ContentTextBox.Text.Trim().Equals("")) { MessageBox.Show("Brak treści docipu!"); return; }
             if (TitleTextBox.Text.Trim().Equals("")) { MessageBox.Show("Brak tytułu docipu!"); return; }
 
             CurrentJoke.Title = TitleTextBox.Text;
             CurrentJoke.Content = ContentTextBox.Text;
-            //CurrentJoke.CreatedDate = (DateTime)CreatedDatePicker.SelectedDate;
+            CurrentJoke.CreatedDate = (DateTime)CreatedDatePicker.SelectedDate;
 
             var json = JsonConvert.SerializeObject(CurrentJoke);
             try
@@ -70,7 +83,7 @@ namespace ClientApp.editWindows
 
                 content.Headers.Remove("Content-Type");
                 content.Headers.Add("Content-Type", "application/json");
-                HttpResponseMessage response = await client.PutAsync("https://localhost:44377/api/jokes/" + CurrentJoke.Id, content);
+                HttpResponseMessage response = await client.PutAsync(ConfigurationManager.AppSettings["ServerURL"] + "jokes/" + CurrentJoke.Id, content);
                 response.EnsureSuccessStatusCode();
             }
             catch (HttpRequestException ex)
@@ -81,11 +94,21 @@ namespace ClientApp.editWindows
             this.Close();
         }
 
+        /// <summary>
+        /// Cancel editing current joke.
+        /// </summary>
+        /// <param name="sender">The control/object that raised the event.</param>
+        /// <param name="e">Event Data.</param>
         private void CancelButton_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
         }
 
+        /// <summary>
+        /// Send deleting current joke http request to server.
+        /// </summary>
+        /// <param name="sender">The control/object that raised the event.</param>
+        /// <param name="e">Event Data.</param>
         private async void DeleteButton_Click(object sender, RoutedEventArgs e)
         {
             try
