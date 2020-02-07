848256f80ac6e3dfa60e51251ac8baf06bdb48cc
Marcin Cyc <marcinbedcyc@mat.umk.pl>
2020-02-07 19:17:12 +0100

    Add documentation to addJokeWindow

--- ClientApp/ClientApp/addWindows/AddJokeWindow.xaml.cs
+++ ClientApp/ClientApp/addWindows/AddJokeWindow.xaml.cs
@@ -33,11 +33,21 @@ namespace ClientApp.addWindows
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Cancel adding new joke to server.
+        /// </summary>
+        /// <param name="sender">The control/object that raised the event</param>
+        /// <param name="e">Event Data</param>
         private void CancelButton_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
         }
 
+        /// <summary>
+        /// Get data from controls and if everything is correct send adding joke to db http request.
+        /// </summary>
+        /// <param name="sender">The control/object that raised the event</param>
+        /// <param name="e">Event Data</param>
         private async void AddButton_Click(object sender, RoutedEventArgs e)
         {
             HttpClient client = new HttpClient();
@@ -53,9 +63,9 @@ namespace ClientApp.addWindows
             {
                 MessageBox.Show(ex.ToString());
             }
-            if (jokes.Where(u => u.Content.Equals(ContentTextBox.Text)).Any()) { MessageBox.Show("Dowcip o podanej treści już istnieje!"); return; }
-            if (TitleTextBox.Text.Trim().Equals("")) { MessageBox.Show("Pusty tytuł"); return; }
-            if (TitleTextBox.Text.Trim().Equals("")) { MessageBox.Show("Pusta zawartośc"); return; }
+            if (!CheckUniqueJokeContent(jokes, ContentTextBox.Text)) { MessageBox.Show("Dowcip o podanej treści już istnieje!"); return; }
+            if (!CheckNewJokeTitle(TitleTextBox.Text.Trim())) { MessageBox.Show("Pusty tytuł"); return; }
+            if (!CheckNewJokeContent(ContentTextBox.Text.Trim())) { MessageBox.Show("Pusta zawartośc"); return; }
 
             Joke joke = new Joke
             {
@@ -81,6 +91,47 @@ namespace ClientApp.addWindows
             {
                 MessageBox.Show(ex.ToString());
             }
+        }
+
+        /// <summary>
+        /// Check if joke's title is not empty.
+        /// </summary>
+        /// <param name="title">Joke's title.</param>
+        /// <returns>True if title is not empty.</returns>
+        public bool CheckNewJokeTitle(string title)
+        {
+            if (title.Equals(""))
+                return false;
+            else
+                return true;
+        }
+
+        /// <summary>
+        /// Check if joke's content is not empty.
+        /// </summary>
+        /// <param name="title">Joke's content.</param>
+        /// <returns>True if content is not empty.</returns>
+        public bool CheckNewJokeContent(string content)
+        {
+            if (content.Equals("")) 
+                return false;
+            else 
+                return true;
+        }
+
+
+        /// <summary>
+        /// Check if joke's content is unique in jokes list.
+        /// </summary>
+        /// <param name="jokes">List of jokes.</param>
+        /// <param name="content">Joke's content.</param>
+        /// <returns>True if content is unique.</returns>
+        public bool CheckUniqueJokeContent(List<Joke> jokes, string content)
+        {
+            if (jokes.Where(u => u.Content.Equals(content)).Any())
+                return false;
+            else
+                return true;
         }
     }
 }
