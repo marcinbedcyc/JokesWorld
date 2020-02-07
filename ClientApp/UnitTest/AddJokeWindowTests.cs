a6c81e9742c5bdf378ce0befb1b8f723741f58ff
Marcin Cyc <marcinbedcyc@mat.umk.pl>
2020-02-07 19:01:59 +0100

    Add testing for addJokeWindow methods

---
+++ ClientApp/UnitTest/AddJokeWindowTests.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClientApp.addWindows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClientApp.models;
+
+namespace ClientApp.addWindows.Tests
+{
+    [TestClass()]
+    public class AddJokeWindowTests
+    {
+        private static AddJokeWindow addJokeWindow = new AddJokeWindow();
+        private static string testingString = "testing string";
+        private static string emptyString = "";
+        private static Joke [] jokesArray = { 
+            new Joke() { Content = testingString, Title = "Title", AuthorFK = 1, Id = 1, CreatedDate = DateTime.Now },
+            new Joke() { Content = "Content", Title = "Title", AuthorFK = 1, Id = 1, CreatedDate = DateTime.Now }
+        };
+        private static List<Joke> jokes = new List<Joke>(jokesArray);
+
+        [TestMethod()]
+        public void CheckNewJokeTitleCorrectTest()
+        {
+            Assert.AreEqual(true, addJokeWindow.CheckNewJokeTitle(testingString));
+        }
+
+        [TestMethod()]
+        public void CheckNewJokeTitleFailTest()
+        {
+            Assert.AreEqual(false, addJokeWindow.CheckNewJokeTitle(emptyString));
+        }
+
+        [TestMethod()]
+        public void CheckNewJokeContentCorrectTest()
+        {
+            Assert.AreEqual(true, addJokeWindow.CheckNewJokeContent(testingString));
+        }
+
+        [TestMethod()]
+        public void CheckNewJokeContentFailTest()
+        {
+            Assert.AreEqual(false, addJokeWindow.CheckNewJokeContent(emptyString));
+        }
+
+        [TestMethod()]
+        public void CheckUniqueJokeContentFailTest()
+        {
+            Assert.AreEqual(false, addJokeWindow.CheckUniqueJokeContent(jokes, testingString));
+        }
+
+        [TestMethod()]
+        public void CheckUniqueJokeContentCorrectTest()
+        {
+            Assert.AreEqual(true, addJokeWindow.CheckUniqueJokeContent(jokes, "Unique string"));
+        }
+    }
+}
